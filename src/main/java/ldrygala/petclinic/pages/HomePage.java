f6ae2152bcd3257218304e11412e98c2a4aedb36
Dominik Markowski <markowski.dominik.j@gmail.com>
2016-03-29 13:50:13 +0200

    create project

--- src/main/java/ldrygala/petclinic/pages/HomePage.java
+++ src/main/java/ldrygala/petclinic/pages/HomePage.java
@@ -1,7 +1,6 @@
 package ldrygala.petclinic.pages;
 
 import ldrygala.petclinic.AbstractPageObject;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
